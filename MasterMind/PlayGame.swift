4a37d37aae7bf5b1555546839aabfe1cecdfff66
chrisTorrecampo <christorrecampo@gmail.com>
2019-04-11 10:21:10 -0700

    Added pages and transitions
    Added:
    -MainMenu
    -DifficultyMenu
    -PlayGame
    -PauseMenu
    -ResultsMenu
    
    -Transitions

---
+++ MasterMind/PlayGame.swift
@@ -0,0 +1,30 @@
+//
+//  PlayGame.swift
+//  MasterMind
+//
+//  Created by Christopher Torrecampo on 4/11/19.
+//  Copyright © 2019 Glenville Pecor. All rights reserved.
+//
+
+import UIKit
+
+class PlayGame: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
